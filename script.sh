ca310a9c321384fbfc42d645c9fa8e8cd0aa1527
Punal Patel <punal.patel@gmail.com>
2017-02-10 15:20:24 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,12 +7,6 @@ echo "$var"
 
 python script.py 
 
-echo "${TRAVIS_BRANCH}"
-echo "${TRAVIS_PULL_REQUEST}"
-echo "${KB_USER}"
-echo "${KB_TEST}"
-echo "${KB_TARGET_URL}"
-
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_BRANCH}" == "prod" ]; then
 		python deploy.py  	
