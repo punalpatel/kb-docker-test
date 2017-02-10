43b83a988957b50c9a3a1c2d2865f2cbbd07e1ce
Punal Patel <punal.patel@gmail.com>
2017-02-10 15:52:16 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,18 +2,6 @@
 
 set -ev
 
-var=$(git diff --name-only HEAD...master)
-echo "$var"
-
-var1=$(git diff --name-only master...HEAD)
-echo "$var1"
-
-var2=$(git diff --name-only HEAD^)
-echo "$var2"
-
-echo "${TRAVIS_PULL_REQUEST}" 
-echo "${TRAVIS_BRANCH}"
-
 python script.py 
 
 # @TODO Add a local variable for non-travis testing
