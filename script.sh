35b88ddbdf375a4e3807d4c841cf28f087915f2a
Timani Tunduwani <timani@users.noreply.github.com>
2017-02-14 11:58:54 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,10 @@ set -ev
 
 python script.py 
 
+git diff --name-only prod..HEAD
+
+git diff --name-only master..HEAD
+
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_BRANCH}" == "prod" ]; then
 		python deploy.py  	
