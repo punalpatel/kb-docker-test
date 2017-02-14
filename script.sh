ccaab64b3135c0c587754759505a2f4e7221fac4
Timani Tunduwani <timani@users.noreply.github.com>
2017-02-14 12:56:24 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ set -ev
 
 python script.py
 
+cat src/KBDockerValidator.py
 
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_BRANCH}" == "prod" ]; then
