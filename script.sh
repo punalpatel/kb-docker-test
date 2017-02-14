5de437d839be43079d10222b394ee6296d4dcfad
Timani Tunduwani <timani@users.noreply.github.com>
2017-02-14 15:10:19 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ set -ev
 python script.py
 
 git branch
+echo "${TRAVIS_BRANCH}"
+
 
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_BRANCH}" == "prod" ]; then
