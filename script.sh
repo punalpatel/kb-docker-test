33e90fd79230552a8056c535480072a006c98811
Timani Tunduwani <timani@users.noreply.github.com>
2017-02-14 12:28:22 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,12 @@
 
 set -ev
 
+var2=$(git diff --name-only HEAD^)
+git diff --name-only HEAD^
+echo "$var2"
+
 python script.py
+
 
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_BRANCH}" == "prod" ]; then
