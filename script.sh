1671c634fef1eb6ea34dc4554d4c0c813c004305
Timani Tunduwani <timani@users.noreply.github.com>
2017-02-14 15:22:56 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,22 @@ python script.py
 git branch
 echo "${TRAVIS_BRANCH}"
 
-git checkout prod
+var=$(git diff --name-only HEAD...master)
+echo "$var"
+
+
+var1=$(git diff --name-only HEAD^...master)
+echo "$var1"
+
+var2=$(git diff --name-only master...HEAD^)
+echo "$var2"
+
+var3=$(git diff --name-only master...HEAD)
+echo "$var3"
+
+
+var3=$(git diff --name-only master...HEAD~1)
+echo "$var3"
 
 # @TODO Add a local variable for non-travis testing
  if [ "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_BRANCH}" == "prod" ]; then
